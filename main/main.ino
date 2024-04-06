0b030693ad3e6239f31f4aee779091b5edf722e6
jaguarclaws2007 <133731134+jaguarclaws2007@users.noreply.github.com>
2024-04-05 21:06:22 -0600

    Update main.ino
    Fixed for Better Mobile Experience, added better project management

--- main/main.ino
+++ main/main.ino
@@ -20,8 +20,7 @@ byte colPins[COLS] = {26, 25, 33, 32};
 
 Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS );
 
-const char* ssid = "CHANGE ME"; //Enter your wifi credentials here
-const char* password = "CHANGE ME"; //Enter your wifi credentials here
+
 
 int mainDelay = 3000;
 
@@ -42,11 +41,62 @@ String color[MAX_TEXTS] = {"placeholder 1", "placeholder 2", "placeholder 3", "p
 String filePrintTime[MAX_TEXTS] = {"placeholder 1", "placeholder 2", "placeholder 3", "placeholder 4", "placeholder 5", "placeholder 6", "placeholder 7", "placeholder 8", "placeholder 9", "placeholder 10"};
 String filamentType[MAX_TEXTS] = {"placeholder 1", "placeholder 2", "placeholder 3", "placeholder 4", "placeholder 5", "placeholder 6", "placeholder 7", "placeholder 8", "placeholder 9", "placeholder 10"};
 String otherInfo[MAX_TEXTS] = {"placeholder 1", "placeholder 2", "placeholder 3", "placeholder 4", "placeholder 5", "placeholder 6", "placeholder 7", "placeholder 8", "placeholder 9", "placeholder 10"};
+String lastEdited[MAX_TEXTS] = {"never", "never", "never", "never", "never", "never", "never", "never", "never", "never"};
 
-String defaultHome[MAX_TEXTS] = {"Is Filament the Right Color", "Is the temperature Pre-Set", "Does the File need supports", "Does the file need a brim", "Is the file going to run over night", "[D] = set delay", "[C] = clear/home", "[#] = enter, [A] = Show IP", "Please keep powered on always", "IP address: " + WiFi.localIP().toString()};
+String defaultHome[MAX_TEXTS] = {"Is Filament the Right Color", "Is the temperature Pre-Set", "Does the File need supports", "Does the file need a brim", "Is the file going to run over night", "[C] = clr/home, [D] = set delay", "[#] = enter, [A] = Show IP", "Please keep powered on always", "IP address: " + WiFi.localIP().toString(), "NAP"};
+
+int amtOfActiveProjects = 0;
+String activeProjects[MAX_TEXTS] = {"No Active Projects via activeProjects"};
+bool showingActiveProjects = true;
 
 int currentTextIndex = 0;
 
+bool activelyShowingProjects = false;
+
+String urlEncode(const String &str) {
+  String encodedString = "";
+  char c;
+  char code0;
+  char code1;
+  for (unsigned int i = 0; i < str.length(); i++) {
+    c = str.charAt(i);
+    if (c == ' ') {
+      encodedString += '+';
+    } else if ((c >= '0' && c <= '9') ||
+               (c >= 'A' && c <= 'Z') ||
+               (c >= 'a' && c <= 'z')) {
+      encodedString += c;
+    } else {
+      code1 = (c & 0xf) + '0';
+      if ((c & 0xf) > 9) {
+        code1 = (c & 0xf) - 10 + 'A';
+      }
+      c = (c >> 4) & 0xf;
+      code0 = c + '0';
+      if (c > 9) {
+        code0 = c - 10 + 'A';
+      }
+      encodedString += '%';
+      encodedString += code0;
+      encodedString += code1;
+    }
+  }
+  encodedString.replace("+", "%20");
+  return encodedString;
+}
+
+void getActiveProjects() {
+  int currentIndex = 0;
+  for (int i = 0; i < MAX_TEXTS; i++) {
+    if (lastEdited[i] != "never") {
+      activeProjects[currentIndex] = "Press " + String(i) + " for: " + projectName[i];
+      currentIndex++;
+    }
+  }
+  amtOfActiveProjects = currentIndex;
+}
+
+#include "credentials.h"
 #include "webpages.h"
 
 void setup() {
@@ -74,6 +124,9 @@ void setup() {
   server.on("/setInfo", HTTP_GET, handleSetInfoProject);
   server.on("/showInfo", HTTP_GET, handleShowInfo);
 
+  server.on("/CHPP", HTTP_GET, handleChangeHomePresetsPage);
+  server.on("/CHP", HTTP_POST, handleChangeHomePresets);
+
   server.begin();
   Serial.println("Server started");
 
@@ -81,7 +134,7 @@ void setup() {
   lcd.print("Connected to WiFi");
   lcd.setCursor(0, 1);
   lcd.print(WiFi.localIP().toString());
-  defaultHome[9] = WiFi.localIP().toString();
+  defaultHome[8] = WiFi.localIP().toString();
   delay(2000);
 }
 
@@ -132,6 +185,7 @@ void loop() {
       Serial.println(setDelay);
     } else if (String(key) == "C") {
       show = "home";
+      page = 0;
     }
   }
   server.handleClient();
@@ -146,35 +200,54 @@ void showInfoOnScreen() {
 
     if (show == "home") {
       // Display default home text
-      lcd.clear();
-      lcd.print(defaultHome[currentTextIndex].substring(0, 16));
-      lcd.setCursor(0, 1);
-      lcd.print(defaultHome[currentTextIndex].substring(16));
+      if ((currentTextIndex == 9 && showingActiveProjects == true) || (activelyShowingProjects == true)) {
+        activelyShowingProjects = true;
+        if (page >= 0 && page <= amtOfActiveProjects-1) {
+          Serial.println("Showing Progress List");
+          Serial.println(page);
+          Serial.println(amtOfActiveProjects);
+          Serial.println(activeProjects[page]);
 
-      currentTextIndex = (currentTextIndex + 1) % MAX_TEXTS;
+          lcd.clear();
+          lcd.print(activeProjects[page].substring(0, 16));
+          lcd.setCursor(0, 1);
+          lcd.print(activeProjects[page].substring(16));
+          page++;
+        } else {
+          page = 0;
+          activelyShowingProjects = false;
+          currentTextIndex = (currentTextIndex + 1) % MAX_TEXTS;
+        }
+      } else {
+        lcd.clear();
+        lcd.print(defaultHome[currentTextIndex].substring(0, 16));
+        lcd.setCursor(0, 1);
+        lcd.print(defaultHome[currentTextIndex].substring(16));
+        currentTextIndex = (currentTextIndex + 1) % MAX_TEXTS;
+      }
     } else if (show >= "0" && show <= "9") {
       page++;
       int projectIndex = show.toInt();
 
       if (page == 1) {
-        printProjectPage(projectName[projectIndex], ", colr:", color[projectIndex]);
+        printProjectPage(projectName[projectIndex], "", "C: " + color[projectIndex]);
 
       } else if (page == 2) {
-        printProjectPage(projectName[projectIndex], ", prt tme:", filePrintTime[projectIndex]);
+        printProjectPage(projectName[projectIndex], "", "T: " + filePrintTime[projectIndex]);
 
       } else if (page == 3) {
-        printProjectPage(projectName[projectIndex], ", Fil tpe:", filamentType[projectIndex]);
+        printProjectPage(projectName[projectIndex], "", "FT: " + filamentType[projectIndex]);
 
       } else if (page == 4) {
-        String info = otherInfo[projectIndex];
+        String info = "O: " + otherInfo[projectIndex];
         int infoLength = info.length();
         int displayLength = 16;
         for (int i = 0; i <= infoLength - displayLength; i++) {
-          printProjectPage(projectName[projectIndex], ", othr:", info.substring(i, i + displayLength));
+          printProjectPage(projectName[projectIndex], "", info.substring(i, i + displayLength));
           delay(500);
         }
         if (infoLength % displayLength != 0) {
-          printProjectPage(projectName[projectIndex], ", othr:", info.substring(infoLength - (infoLength % displayLength)));
+          printProjectPage(projectName[projectIndex], "", info.substring(infoLength - (infoLength % displayLength)));
         }
 
       } else if (page == 5) {
@@ -184,7 +257,7 @@ void showInfoOnScreen() {
         lcd.print("for more info");
 
       } else if (page == 6) {
-        String message = "or goto: " + WiFi.localIP().toString() + "/qrCode";
+        String message = "Or goto: " + WiFi.localIP().toString() + "/qrCode";
 
         lcd.clear();
         lcd.print(message.substring(0, 16));
